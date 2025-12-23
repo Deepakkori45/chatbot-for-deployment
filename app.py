b994f04d00b004f1606a814fa52f811bbc2e152d
Deepak kori <111627339+Deepakkori45@users.noreply.github.com>
2025-12-23 14:27:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import logging
 # Load environment variables 
 load_dotenv()
 
+
 # Configuration
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 ASSISTANT_ID = os.getenv("ASSISTANT_ID", "asst_PytLeS8CwhZiswnc11HCsmbO")
@@ -16,7 +17,7 @@ ASSISTANT_SYSTEM_PROMPT = (
     "You are a helpful assistant. Rely solely on the supplied knowledge base. "
     "If the answer isn’t there, reply: ‘I’m sorry, that information isn’t in my database. "
     "Please re-ask using topics the database covers. Keep every reply directly focused on the question. "
-    "Present the reply as a numbered or bulleted list.’"
+    "Present the reply as a numbered or bulletedlist.’"
 ) 
 POLLING_INTERVAL = 1
 
